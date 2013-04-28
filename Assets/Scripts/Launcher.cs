a5d46ee4010bdadd855a87267be94ebd61cf8dcc
Rob Caporetto <rcaporetto@gmail.com>
2013-04-28 23:41:28 +1000

    Reworked firing code to be auto fire, instead of manually tapping the fire key.

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -8,13 +8,40 @@ public class Launcher : MonoBehaviour
 	[SerializeField] private float bulletLife = 2f;
 	[SerializeField] private float bulletForce = 2f;
 	
-	public void FireBullet()
+	private bool isFiring;
+	
+	void Awake()
 	{
+		isFiring = false;
+	}
+	
+	public void StartAutoFire()
+	{
+		if (!isFiring)
+			StartCoroutine("FireRoutine");
+	}
+	
+	public void StopAutoFire()
+	{
+		if (!isFiring) return;
+
+		StopCoroutine("FireRoutine");
+		isFiring = false;
+	}
+	
+	private IEnumerator FireRoutine()
+	{
+		isFiring = true;
+		
+		StartFiring:
 		GameObject bullet = (GameObject)Instantiate(bulletPrefab);
 		bullet.transform.rotation = transform.rotation;
 		bullet.transform.position = transform.position + bullet.transform.TransformDirection(launchOffset);
 		bullet.rigidbody.AddForce(bullet.transform.TransformDirection(Vector3.forward) * bulletForce);
 
 		Destroy(bullet, bulletLife);
+		
+		yield return new WaitForSeconds(0.166666667f);
+		goto StartFiring;
 	}
 }
