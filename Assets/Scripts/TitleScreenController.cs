c90cb9da47b3ca46ae2b8f985b4526b1b53349b6
Rob Caporetto <rcaporetto@gmail.com>
2013-04-28 20:42:28 +1000

    Tweaking bullet & player colliders. Adding credits to game complete screen.

--- Assets/Scripts/TitleScreenController.cs
+++ Assets/Scripts/TitleScreenController.cs
@@ -7,19 +7,24 @@ public class TitleScreenController : MonoBehaviour
 	[SerializeField] private TextMesh startGameActionLabel;
 	[SerializeField] private TextMesh instructionsCreditsLabel;
 	[SerializeField] private TextMesh gameOverLabel;
+	[SerializeField] private TextMesh gameCompleteMessageLabel;
+	[SerializeField] private TextMesh gameCompleteTitleLabel;
+	[SerializeField] private TextMesh creditsLabel;
+	
 	[SerializeField] private TextMesh scoreLabel;
 	[SerializeField] private TextMesh highScoreLabel;
 	[SerializeField] private TextMesh waveLabel;
-	[SerializeField] private TextMesh gameCompleteTitleLabel;
-	[SerializeField] private TextMesh gameCompleteMessageLabel;
-	
+
 	public void ShowTitleScreen()
 	{
 		titleLabel.gameObject.SetActive(true);
 		startGameActionLabel.gameObject.SetActive(true);
+		// instructionsCreditsLabel.gameObject.SetActive(true);
+		
 		gameOverLabel.gameObject.SetActive(false);
 		gameCompleteTitleLabel.gameObject.SetActive(false);
 		gameCompleteMessageLabel.gameObject.SetActive(false);
+		creditsLabel.gameObject.SetActive(false);
 	}
 	
 	public void ShowInGameScreen()
@@ -27,26 +32,34 @@ public class TitleScreenController : MonoBehaviour
 		titleLabel.gameObject.SetActive(false);
 		startGameActionLabel.gameObject.SetActive(false);
 		gameOverLabel.gameObject.SetActive(false);
+
 		gameCompleteTitleLabel.gameObject.SetActive(false);
 		gameCompleteMessageLabel.gameObject.SetActive(false);
+		creditsLabel.gameObject.SetActive(false);
 	}
 	
 	public void ShowGameOverScreen()
 	{
+		gameOverLabel.gameObject.SetActive(true);
+
 		titleLabel.gameObject.SetActive(false);
 		startGameActionLabel.gameObject.SetActive(false);
-		gameOverLabel.gameObject.SetActive(true);
+		
 		gameCompleteTitleLabel.gameObject.SetActive(false);
 		gameCompleteMessageLabel.gameObject.SetActive(false);
+		creditsLabel.gameObject.SetActive(false);
 	}
 	
 	public void ShowGameCompleteScreen()
 	{
+		gameOverLabel.gameObject.SetActive(false);
+		
 		titleLabel.gameObject.SetActive(false);
 		startGameActionLabel.gameObject.SetActive(false);
-		gameOverLabel.gameObject.SetActive(false);
+		
 		gameCompleteTitleLabel.gameObject.SetActive(true);
 		gameCompleteMessageLabel.gameObject.SetActive(true);
+		creditsLabel.gameObject.SetActive(true);
 	}
 	
 	public void UpdateScoreAndWaveLabels(int score, int wave, int hiScore)
